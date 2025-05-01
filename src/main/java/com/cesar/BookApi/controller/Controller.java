775703d417218f18c05cc2cbedb91a61b65ba0be
CsarNarciso <cesarpazol1029@gmail.com>
2025-05-01 16:59:04 -0600

    Fix missing return statements in service methods, replace wrong service methods naming in controller

--- src/main/java/com/cesar/BookApi/controller/Controller.java
+++ src/main/java/com/cesar/BookApi/controller/Controller.java
@@ -1,17 +1,13 @@
 package com.cesar.BookApi.controller;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,14 +20,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cesar.BookApi.dto.Book_DTO;
-import com.cesar.BookApi.entity.Book;
 import com.cesar.BookApi.service.BookService;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
 
 @RestController
 @RequestMapping(value = "/books.api", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -42,7 +33,7 @@ public class Controller {
 	private ResponseEntity<?> getAll(){
 		
 		//Mapping list entity books to DTO
-		List<Book_DTO> books = service.findAll();
+		List<Book_DTO> books = service.getAll();
 		
 		//If there's books...
 		if ( ! books.isEmpty() ) {
@@ -58,11 +49,10 @@ public class Controller {
 	@GetMapping("/books/{book_id}")
 	private ResponseEntity<?> getById(@PathVariable Long book_id) {
 		
-		Book_DTO book = service.findById(book_id);
+		Book_DTO book = service.getById(book_id);
 		
 		//If book exist..
-		if ( book.isPresent() ) {
-			
+		if (book != null) {
 			return ResponseEntity.ok( book );
 		}
 		return ResponseEntity.noContent().build();
@@ -90,7 +80,7 @@ public class Controller {
 	@PostMapping( value = "/books", consumes = MediaType.APPLICATION_JSON_VALUE)
 	private ResponseEntity<?> create(@RequestBody @Valid Book_DTO book) {
 		
-		book = service.save(book); 
+		book = service.create(book);
 		return ResponseEntity.status(HttpStatus.CREATED).body( book );
 	}
 	
@@ -102,7 +92,7 @@ public class Controller {
 	private ResponseEntity<?> replace(@PathVariable Long book_id, @RequestBody @Valid Book_DTO replaceBook) {
 		
 		//if this book already exists..
-		if (service.findById(book_id) != null) {
+		if (service.getById(book_id) != null) {
 		
 			replaceBook = service.replace(book_id, replaceBook);
 			return ResponseEntity.ok( replaceBook );
@@ -117,7 +107,7 @@ public class Controller {
 	private ResponseEntity<?> update(@PathVariable Long book_id, @RequestBody Map<String, Object> fields) {
 		
 		//if this book already exists..
-		if (service.findById(book_id) != null) {
+		if (service.getById(book_id) != null) {
 		
 			Book_DTO updatedBook = service.update(book_id, fields);
 			return ResponseEntity.ok(updatedBook);
@@ -131,13 +121,13 @@ public class Controller {
 	@DeleteMapping("/books/{book_id}")
 	public ResponseEntity<?> delete(@PathVariable Long book_id) {
 		
-		Book_DTO book = service.findById(book_id);
+		Book_DTO book = service.getById(book_id);
 		
 		//if this book exists..
 		if (book != null) {
 					
 			//Delete from DB
-			service.deleteById(book_id);
+			service.delete(book_id);
 			return ResponseEntity.ok().build();
 		}
 		return ResponseEntity.noContent().build();	
--- src/main/java/com/cesar/BookApi/service/BookService.java
+++ src/main/java/com/cesar/BookApi/service/BookService.java
@@ -2,79 +2,149 @@ package com.cesar.BookApi.service;
 
 import com.cesar.BookApi.dto.Book_DTO;
 import com.cesar.BookApi.entity.Book;
+import com.cesar.BookApi.repository.Book_Repository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
-import java.util.List;
-import java.util.Optional;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @Service
 public class BookService {
 
+	public List<Book_DTO> getAll() {
 
-    private final Service service;
-
-    public BookService(Service service) {
-        this.service = service;
-    }
-
-    private List<Book_DTO> getAll() {
-
-        //Mapping list entity books to DTO
-        return service.bookRepo.findAll().stream()
-                .map(bookEntity -> service.modelMapper.map(bookEntity, Book_DTO.class))
-                .toList();
-    }
+		//Mapping list entity books to DTO
+		return bookRepo.findAll().stream()
+				.map(bookEntity -> modelMapper.map(bookEntity, Book_DTO.class))
+				.toList();
+	}
 
 
-    private Book_DTO getById(Long book_id) {
+	public Book_DTO getById(Long book_id) {
 
-        //Searching book entity by id
-        Optional<Book> bookEntity = service.bookRepo.findById(book_id);
+		//Searching book entity by id
+		Optional<Book> bookEntity = bookRepo.findById(book_id);
 
-        //If book exist..
-        if (bookEntity.isPresent()) {
+		//If book exist..
+		if (bookEntity.isPresent()) {
 
-            //Mapping entity to DTO
-            return service.modelMapper.map(bookEntity.get(), Book_DTO.class);
-        }
-        return null;
-    }
-
-
-    private List<Book_DTO> getByGenre(String genre) {
-
-        if (!genre.isEmpty()) {
-
-            //Mapping list entity books to DTO
-            List<Book_DTO> books = service.bookRepo.getByGenre(genre).stream()
-                    .map(bookEntity -> service.modelMapper.map(bookEntity, Book_DTO.class))
-                    .toList();
-
-            return books;
-        }
-    }
+			//Mapping entity to DTO
+			return modelMapper.map(bookEntity.get(), Book_DTO.class);
+		}
+		return null;
+	}
 
 
-    private Book_DTO create(Book_DTO book) {
+	public List<Book_DTO> getByGenre(String genre) {
 
-        //Set id in null
-        book.setId(null);
+		List<Book_DTO> books = Collections.emptyList();
 
-        //Mapping DTO to entity to register in DB,
-        book = service.modelMapper.map(service.bookRepo.save(service.modelMapper.map(book, Book.class)), Book_DTO.class); //and re-mapping result to DTO
+		if (!genre.isEmpty()) {
 
-        return book;
-    }
+			//Mapping list entity books to DTO
+			books = bookRepo.getByGenre(genre).stream()
+					.map(bookEntity -> modelMapper.map(bookEntity, Book_DTO.class))
+					.toList();
+		}
+		return books;
+	}
 
 
-    private Book_DTO replace(Long book_id, Book_DTO replaceBook) {
+	public Book_DTO create(Book_DTO book) {
+
+		//Set id in null
+		book.setId(null);
+
+		//Mapping DTO to entity to register in DB,
+		book = modelMapper.map(bookRepo.save(modelMapper.map(book, Book.class)), Book_DTO.class); //and re-mapping result to DTO
+
+		return book;
+	}
 
 
-        //Set id_book on update data to prevent replacing other book
-        replaceBook.setId(book_id);
+	public Book_DTO replace(Long book_id, Book_DTO replaceBook) {
 
-        //Mapping DTO to entity to replace in BBDD,
-        replaceBook = service.modelMapper.map(service.bookRepo.save(service.modelMapper.map(replaceBook, Book.class)), Book_DTO.class); //and re-mapping to DTO
 
-        return replaceBook;
-    }
+		//Set id_book on update data to prevent replacing other book
+		replaceBook.setId(book_id);
+
+		//Mapping DTO to entity to replace in DB,
+		replaceBook = modelMapper.map(bookRepo.save(modelMapper.map(replaceBook, Book.class)), Book_DTO.class); //and re-mapping to DTO
+
+		return replaceBook;
+	}
+
+
+	public Book_DTO update(Long book_id, Map<String, Object> fields) {
+
+		Optional<Book> optionalBook = bookRepo.findById(book_id);
+
+		//if this book already exists..
+		if (optionalBook.isPresent()) {
+
+			Book book = optionalBook.get();
+
+			//Remove id to prevent change in entity.
+			fields.remove("id");
+
+
+			//Set change fields to Entity
+			fields.forEach((k, v) -> {
+
+				Field field = ReflectionUtils.findField(Book.class, k);
+
+				if (field != null) {
+
+					field.setAccessible(true);
+					ReflectionUtils.setField(field, book, v);
+				}
+			});
+
+			//Mapping to DTO to validate
+			Book_DTO dtoValidate = modelMapper.map(book, Book_DTO.class);
+
+
+			//Validate
+			Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+			Set<ConstraintViolation<Book_DTO>> violations = validator.validate(dtoValidate);
+
+			if (!violations.isEmpty()) {
+
+				throw new ConstraintViolationException(violations);
+			}
+
+			//Save change entity to update in DB,
+			Book_DTO updateBook = modelMapper.map(bookRepo.save(book), Book_DTO.class); //and re-mapping to DTO
+
+			return updateBook;
+		}
+		return null;
+	}
+
+
+	public void delete(Long book_id) {
+
+		Optional<Book> optionalBook = bookRepo.findById(book_id);
+
+		//if this book exists..
+		if (optionalBook.isPresent()) {
+
+			//Delete from DB
+			bookRepo.deleteById(book_id);
+		}
+	}
+
+
+	@Autowired
+	private Book_Repository bookRepo;
+
+	@Autowired
+	private ModelMapper modelMapper;
 }
