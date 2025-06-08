d28045fe8687f0a8501bc9b39e5e9b2c0b51fea9
CsarNarciso <cesarpazol1029@gmail.com>
2025-06-08 10:52:16 -0600

    Fix request loop on failure due to use controllerAdvice instead RestControllerAdvice

--- src/main/java/com/cesar/SpringBootAPI/advice/GlobalAdvice.java
+++ src/main/java/com/cesar/SpringBootAPI/advice/GlobalAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -21,7 +21,7 @@ import jakarta.validation.ConstraintViolationException;
 
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalAdvice{
 
 	@ExceptionHandler(NoContentException.class)
